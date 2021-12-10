b60853adf6a464e969c62f8bd52c9183c2fe1795
baijin <jinwarr@gmail.com>
2021-12-10 19:44:36 +0800

    axum first

--- README.md
+++
@@ -1,1 +0,0 @@
-# axum_demo
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,37 +1,73 @@
-use std::net::SocketAddr;
+use std::{collections::HashMap, net::SocketAddr, str::FromStr};
 
-use demo::hello::{
-    greeter_server::{Greeter, GreeterServer},
-    HelloRequest, HelloResponse,
+use axum::{
+    extract::{Form, Path},
+    handler::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
 };
+use serde::{Deserialize, Serialize};
 use tokio::runtime::Builder;
-use tonic::{transport::Server, Request, Response, Status};
-
-struct MyGreetter {}
-
-#[tonic::async_trait]
-impl Greeter for MyGreetter {
-    async fn say_hello(
-        &self,
-        req: Request<HelloRequest>,
-    ) -> Result<Response<HelloResponse>, Status> {
-        println!("收到调用", );
-        Ok(Response::new(HelloResponse {
-            greeting: format!("hello,{}!", req.get_ref().name),
-        }))
-    }
-}
-
 fn main() {
     let rt = Builder::new_multi_thread().enable_all().build().unwrap();
-    rt.block_on(async {
-        let addr: SocketAddr = "127.0.0.1:5001".parse().unwrap();
-        let greeter = MyGreetter {};
 
-        Server::builder()
-            .add_service(GreeterServer::new(greeter))
-            .serve(addr)
+    rt.block_on(async {
+        let app = Router::new()
+            .route("/", get(root))
+            .route("/json", post(create_user_json))
+            .route("/url/:name/:age", get(create_user_get))
+            .route("/from", post(create_user_from));
+        let addr = SocketAddr::from_str("127.0.0.1:3000").unwrap();
+        axum::Server::bind(&addr)
+            .serve(app.into_make_service())
             .await
             .unwrap();
     })
+}
+
+async fn root() -> &'static str {
+    "hello"
+}
+
+async fn create_user_json(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1000,
+        name: payload.name,
+        age: payload.age,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+
+async fn create_user_get(Path(params): Path<HashMap<String, String>>) -> impl IntoResponse {
+    let name = params.get("name").unwrap().clone();
+    let age = params.get("age").unwrap().parse().unwrap();
+    let user = User {
+        id: 1000,
+        name,
+        age,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+
+async fn create_user_from(Form(from): Form<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1000,
+        name: from.name,
+        age: from.age,
+    };
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    name: String,
+    age: i32,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+    age: i32,
 }
