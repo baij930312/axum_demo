05b3019dd41763eb32a601084fa3bd49f0310165
baijin2966 <baijin@ftsafe.com>
2021-12-17 14:50:29 +0800

    tokio Notify

---
+++ src/bin/delay.rs
@@ -0,0 +1,24 @@
+use std::sync::Arc;
+use std::thread;
+use std::time::{Duration, Instant};
+use tokio::sync::Notify;
+
+async fn delay(dur: Duration) {
+    let when = Instant::now() + dur;
+    let notify = Arc::new(Notify::new());
+    let notify2 = notify.clone();
+    thread::spawn(move || {
+        let now = Instant::now();
+        if now < when {
+            thread::sleep(when - now);
+        }
+        notify2.notify_one();
+    });
+
+    notify.notified().await;
+}
+
+#[tokio::main]
+async fn main() {
+    delay(Duration::from_secs(10)).await;
+}
