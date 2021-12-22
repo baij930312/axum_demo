170f38b9f8536c2e9d0bff6cd3b493377138522e
baijin2966 <baijin@ftsafe.com>
2021-12-22 15:56:37 +0800

    static file ，upload，

--- src/bin/middleware.rs
+++ src/bin/middleware.rs
@@ -1,10 +1,10 @@
 use axum::{
     body::Body,
-    extract::{Extension, Form, Path},
+    extract::{ContentLengthLimit, Extension, Form, Multipart, Path},
     http::{Request, StatusCode},
     response::IntoResponse,
-    response::Response,
-    routing::{get, post},
+    response::{Html, Response},
+    routing::{get, get_service, post},
     AddExtensionLayer, Json, Router,
 };
 use futures::future::BoxFuture;
@@ -17,7 +17,8 @@ use std::{
     task::{Context, Poll},
 };
 use tokio::runtime::Builder;
-use tower::{layer::layer_fn, Service};
+use tower::{layer::layer_fn, Service, ServiceBuilder};
+use tower_http::services::ServeDir;
 
 struct State {
     data: String,
@@ -27,17 +28,37 @@ fn main() {
     let rt = Builder::new_multi_thread().enable_all().build().unwrap();
 
     rt.block_on(async {
+        if std::env::var_os("RUST_LOG").is_none() {
+            std::env::set_var("RUST_LOG", "example_multipart_form=debug,tower_http=debug")
+        }
+        tracing_subscriber::fmt::init();
         let state = AddExtensionLayer::new(Arc::new(State {
             data: "asdasdsa".into(),
         }));
 
+        let middleware_stack = ServiceBuilder::new()
+            // .layer(layer_fn(|inner| MyMiddleware { inner }))
+            .layer(state)
+            .layer(tower_http::trace::TraceLayer::new_for_http());
+
         let app = Router::new()
             .route("/", get(root))
+            .route("/upload", get(upload_page).post(accept_form))
             .route("/json", post(create_user_json))
             .route("/url/:name/:age", get(create_user_get))
             .route("/from", post(create_user_from))
-            .layer(state)
-            .layer(layer_fn(|inner| MyMiddleware { inner }));
+            .nest(
+                "/static",
+                get_service(ServeDir::new("./static")).handle_error(
+                    |error: std::io::Error| async move {
+                        (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("Unhandled internal error: {}", error),
+                        )
+                    },
+                ),
+            )
+            .layer(middleware_stack);
 
         let addr = SocketAddr::from_str("127.0.0.1:3000").unwrap();
         axum::Server::bind(&addr)
@@ -47,7 +68,44 @@ fn main() {
     })
 }
 
-async fn root() -> &'static str {
+async fn upload_page() -> Html<&'static str> {
+    Html(
+        r#"
+        <!doctype html>
+        <html>
+            <head></head>
+            <body>
+                <form action="/upload" method="post" enctype="multipart/form-data">
+                    <label>
+                        Upload file:
+                        <input type="file" name="file" multiple>
+                    </label>
+                    <input type="submit" value="Upload files">
+                </form>
+            </body>
+        </html>
+        "#,
+    )
+}
+
+async fn accept_form(
+    ContentLengthLimit(mut multipart): ContentLengthLimit<
+        Multipart,
+        {
+            250 * 1024 * 1024 /* 250mb */
+        },
+    >,
+) {
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        println!("Length of `{}` is {} bytes", name, data.len());
+    }
+}
+
+async fn root(Extension(state): Extension<Arc<State>>) -> &'static str {
+    println!("{}", state.data);
     "hello"
 }
 
